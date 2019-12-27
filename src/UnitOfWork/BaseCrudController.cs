7656a40e36914d12d2e1a129537babcd2694bc50
Rodrigo Diniz <ext.rodrigo.diniz@farfetch.com>
2019-12-27 14:49:04 +0000

    Adding fluent validation as a dependency

--- src/UnitOfWork/BaseCrudController.cs
+++ src/UnitOfWork/BaseCrudController.cs
@@ -7,7 +7,9 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
-    public class BaseCrudController<M, T> : ControllerBase where T : class
+    [ApiController]
+    [Route("api/[controller]")]
+    public abstract class BaseCrudController<M, T> : ControllerBase where T : class
     {
         private readonly IUnitOfWork _unitOfWork;
 
